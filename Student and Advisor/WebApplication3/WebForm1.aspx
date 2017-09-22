153f9be19def2b8eef365d47ace078595dc32aad
vishnusasuke <vishnusasuke@gmail.com>
2017-09-22 14:15:39 -0500

    initial commit

---
+++ Student and Advisor/WebApplication3/WebForm1.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+    <h3>Details</h3>
+    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Student and Advisor/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //string confString = WebConfigurationManager.ConnectionStrings["StudentCon"].ConnectionString;
+        //public void LoadGridview()
+        //{
+        //    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["confString"].ConnectionString);
+        //    SqlCommand cmd = new SqlCommand("Select tblStudent.Name, tblAdbisor.Name from tblStudent join tblAdvisor where tblStudent.StudentId == tblAdvisor.AdvisorId");
+        //    using(con)
+        //    {
+        //        con.Open();
+        //        cmd.ExecuteNonQuery();
+        //        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+        //        {
+        //            DataTable dt = new DataTable();
+        //            da.Fill(dt);   
+        //            GridView1.DataSource= da;
+        //            GridView1.DataBind();
+        //        }
+        //    }
+        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String databaseDetails = "Data Source=.;Initial Catalog=StudentsDB;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(databaseDetails);
+            
+            string query = " select tblStudent.Name, tblAdvisor.Name from tblStudent left join tblAdvisor on tblStudent.AdvisiorId = tblAdvisor.AdvisorId order by tblStudent.Name ;";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader readData =    command.ExecuteReader();
+            GridView1.DataSource = readData;
+            GridView1.DataBind();
+            connection.Close();
+
+        }
+    }
+}
